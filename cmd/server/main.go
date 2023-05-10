5add34a3eb2deca93aea83fadf0d76188cfcbabd
zachmdsi <zmcmanus@dsi.us>
2023-05-10 18:25:23 -0500

    deal with closing connections better + fix server interrupt bug

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,8 +8,11 @@ import (
 	"os"
 	"os/signal"
 	"strings"
+	"sync"
 	"syscall"
+	"time"
 )
+
 
 func main() {
 	ln, err := net.Listen("tcp", ":8080")
@@ -20,24 +23,54 @@ func main() {
 
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	var connections []net.Conn
+	var mutex sync.Mutex
+	var shuttingDown bool
+
 	go func() {
 		sig := <-signals
-		fmt.Printf("Received signal %v, shutting down...", sig)
+		fmt.Printf("Received signal %v, shutting down...\n", sig)
+		shuttingDown = true
 		ln.Close()
+		mutex.Lock()
+		for _, conn := range connections {
+			conn.Close()
+		}
+		mutex.Unlock()
 	}()
 
 	for {
+		if shuttingDown {
+			fmt.Println("Server is shutting down...")
+			break
+		}
 		conn, err := ln.Accept()
 		if err != nil {
-			fmt.Printf("Error accepting connection: %v", err)
+			if !shuttingDown {
+				fmt.Printf("Error accepting connection: %v\n", err)
+			}
 			continue
 		}
-		go handleConnection(conn, "data/ais-sample-data.csv", 1000)
+		mutex.Lock()
+		connections = append(connections, conn)
+		mutex.Unlock()
+		go handleConnection(conn, "data/ais-sample-data.csv", 100, &connections, &mutex)
 	}
 }
 
-func handleConnection(conn net.Conn, filename string, batchSize int) {
-    defer conn.Close()
+func handleConnection(conn net.Conn, filename string, batchSize int, connections *[]net.Conn, mutex *sync.Mutex) {
+	defer func() {
+		conn.Close()
+		mutex.Lock()
+		for i, c := range *connections {
+			if c == conn {
+				*connections = append((*connections)[:i], (*connections)[i+1:]...)
+				break
+			}
+		}
+		mutex.Unlock()
+	}()
 
     // Open the CSV file for reading
     file, err := os.Open(filename)
@@ -59,20 +92,28 @@ func handleConnection(conn net.Conn, filename string, batchSize int) {
     for {
         // Read a record from the CSV file
         record, err := reader.Read()
-        if err == io.EOF {
-            fmt.Println("End of file, closing connection")
-            return
-        }
         if err != nil {
-            fmt.Printf("Error reading line %d: %v\n", lineNumber, err)
+            if err == io.EOF {
+                fmt.Println("End of file, closing connection")
+            } else {
+                fmt.Printf("Error reading line %d: %v\n", lineNumber, err)
+            }
+
+            // Send any remaining records
+            if len(records) > 0 {
+                _, err = conn.Write([]byte(strings.Join(records, "")))
+                if err != nil {
+                    fmt.Printf("Error writing data: %v\n", err)
+                }
+            }
             return
-        }
+        } 
 
         // Append the record to the batch
         data := strings.Join(record, ",") + "\n"
         records = append(records, data)
 
-        // If the batch is full, write it to the client and reset the batch
+        // If the batch is full, write it to the client, reset the batch, and pause for 1 second
         if len(records) == batchSize {
             _, err = conn.Write([]byte(strings.Join(records, "")))
             if err != nil {
@@ -80,6 +121,7 @@ func handleConnection(conn net.Conn, filename string, batchSize int) {
                 return
             }
             records = make([]string, 0, batchSize)
+            time.Sleep(1 * time.Second)
         }
 
         lineNumber++
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/zachmdsi/go-ais-vessel-tracking
 
 go 1.19
+
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
+)
